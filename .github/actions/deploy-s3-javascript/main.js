45f4a98d348457280574b1315b83edd012ffba82
Sahil Batra <shlbatra123@gmail.com>
2023-05-04 17:06:27 -0400

    push code to aws with url logged

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,9 @@ function run(){
     const s3uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom JS action!')
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput('website-url', websiteUrl) // ::set-output
+
 }
 
 run();
